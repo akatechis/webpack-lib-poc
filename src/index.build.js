f4ae7a66c83bf2f81efce5bda5cc9518a37b2ee2
Alexandros Katechis <alex.katechis@pefin.com>
2018-01-30 14:00:47 -0500

    Convert build script into a webpack plugin

--- src/index.build.js
+++ src/index.build.js
@@ -2,28 +2,36 @@ const fs = require("fs");
 const { EOL } = require("os");
 const path = require("path");
 
-let modules = 0;
-const buffer = [
-  "// auto-generated file", "",
-];
+module.exports = function () {
+  this.plugin('run', function(compiler, callback) {
+    console.log("Build script starting");
 
-const emitModule = file => {
-  const moduleName = file.replace(".js", "");
-  modules += 1;
-  buffer.push(`exports.${moduleName} = require("./snippets/${moduleName}");`);
+    let modules = 0;
+    const buffer = [
+      "// auto-generated file", "",
+    ];
+
+    const emitModule = file => {
+      const moduleName = file.replace(".js", "");
+      modules += 1;
+      buffer.push(`exports.${moduleName} = require("./snippets/${moduleName}");`);
+    };
+
+    const files = fs.readdirSync(__dirname + "/snippets");
+
+    files
+    .filter(fname => fname !== "index.js" && !fname.startsWith("."))
+    .forEach(f => {
+      const stats = fs.statSync(path.join(__dirname, "snippets", f));
+      if (stats.isFile()) {
+        emitModule(f);
+      }
+    });
+
+    fs.writeFileSync(path.join(__dirname, "index.js"), buffer.join(EOL)+EOL);
+
+    console.info(`Build script done. 'src/index.js' emitted with ${modules} modules`);
+
+    callback();
+  });
 };
-
-const files = fs.readdirSync(__dirname + "/snippets");
-
-files
-.filter(fname => fname !== "index.js" && !fname.startsWith("."))
-.forEach(f => {
-  const stats = fs.statSync(path.join(__dirname, "snippets", f));
-  if (stats.isFile()) {
-    emitModule(f);
-  }
-});
-
-fs.writeFileSync(path.join(__dirname, "index.js"), buffer.join(EOL)+EOL);
-
-console.info(`Built 'src/index.js' with ${modules} modules`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const buildPlugin = require("./src/index.build");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
@@ -6,5 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, "build/"),
     filename: "mylib.js",
     libraryTarget: "umd"
-  }
+  },
+  plugins: [buildPlugin]
 };
