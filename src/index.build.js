fc26eadbdca3db14a0d0919bc9aa307dc11bb358
Alexandros Katechis <alex.katechis@pefin.com>
2018-01-30 14:03:40 -0500

    Refactor the plugin interface to extract a function

--- src/index.build.js
+++ src/index.build.js
@@ -2,36 +2,38 @@ const fs = require("fs");
 const { EOL } = require("os");
 const path = require("path");
 
+function build() {
+  let modules = 0;
+  const buffer = [
+    "// auto-generated file", "",
+  ];
+
+  const emitModule = file => {
+    const moduleName = file.replace(".js", "");
+    modules += 1;
+    buffer.push(`exports.${moduleName} = require("./snippets/${moduleName}");`);
+  };
+
+  const files = fs.readdirSync(__dirname + "/snippets");
+
+  files
+  .filter(fname => fname !== "index.js" && !fname.startsWith("."))
+  .forEach(f => {
+    const stats = fs.statSync(path.join(__dirname, "snippets", f));
+    if (stats.isFile()) {
+      emitModule(f);
+    }
+  });
+
+  fs.writeFileSync(path.join(__dirname, "index.js"), buffer.join(EOL)+EOL);
+
+  console.info(`Build script done. 'src/index.js' emitted with ${modules} modules`);
+}
+
 module.exports = function () {
   this.plugin('run', function(compiler, callback) {
     console.log("Build script starting");
-
-    let modules = 0;
-    const buffer = [
-      "// auto-generated file", "",
-    ];
-
-    const emitModule = file => {
-      const moduleName = file.replace(".js", "");
-      modules += 1;
-      buffer.push(`exports.${moduleName} = require("./snippets/${moduleName}");`);
-    };
-
-    const files = fs.readdirSync(__dirname + "/snippets");
-
-    files
-    .filter(fname => fname !== "index.js" && !fname.startsWith("."))
-    .forEach(f => {
-      const stats = fs.statSync(path.join(__dirname, "snippets", f));
-      if (stats.isFile()) {
-        emitModule(f);
-      }
-    });
-
-    fs.writeFileSync(path.join(__dirname, "index.js"), buffer.join(EOL)+EOL);
-
-    console.info(`Build script done. 'src/index.js' emitted with ${modules} modules`);
-
+    build();
     callback();
   });
 };
